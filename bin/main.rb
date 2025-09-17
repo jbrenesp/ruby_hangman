52fed9a7fbc5357d42fb0e4adeb55fe375ae4f88
jbrenesp <josembrenes@gmail.com>
2025-09-17 15:22:54 +0300

    Add Hangman class skeleton and dictionary loading with random word selection

---
+++ bin/main.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# bin/main.rb
+
+require_relative "../lib/hangman"
+
+game = Hangman.new
+game.play
---
+++ lib/hangman.rb
@@ -0,0 +1,17 @@
+class Hangman 
+  attr_reader :secret_word
+
+  def initialize(dictionary_file = "google-10000-english-no-swears.txt")
+    puts "Hangman game initialize"
+
+    words = File.readlines(dictionary_file, chomp: true)
+    filtered_words = words.select { |w| w.length.between?(5, 12)}
+
+    @secret_word = filtered_words.sample
+  end
+
+  def play
+    puts "🎮 Game starting..."
+    puts "Secret word has #{@secret_word.length} letters!"
+  end
+end
